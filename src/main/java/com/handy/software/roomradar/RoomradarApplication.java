a35d3305b31304484558a13f75ceb7e53472db5b
Mart-7900X3D-PC <martvandermolen@gmail.com>
2024-06-21 12:50:25 +0200

    Intial

---
+++ src/main/java/com/handy/software/roomradar/RoomradarApplication.java
@@ -0,0 +1,13 @@
+package com.handy.software.roomradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomradarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomradarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/handy/software/roomradar/RoomradarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handy.software.roomradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomradarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
